d43e7d0af92fdbf207a288c05ce8335be0f6d28f
Crystal Williams <cortney0215@gmail.com>
2022-02-11 20:46:34 -0500

    convert promises to async/await

--- routes/thetvdb-api.js
+++ routes/thetvdb-api.js
@@ -6,46 +6,35 @@ const axios = require('axios');
  * @param {*} cookies
  * @return { promise} with the response or error
  */
-function tvShowLogin() {
-  return new Promise((resolve, reject) => {
-    const auth = {
-      apikey: process.env.showApiKey,
-      username: process.env.user,
-      userkey: process.env.uniqueId
-    };
+async function tvShowLogin() {
+	const auth = {
+		apikey: process.env.showApiKey,
+		username: process.env.user,
+		userkey: process.env.uniqueId
+	};
 
-    axios.post('https://api.thetvdb.com/login', auth).then((response) => {
-      console.log('tv api response ', response);
-      resolve(response.data.token);
-    }).catch((err) => {
-      console.log('error ', err);
-      reject(err);
-    });
-  });
+	const response = await axios.post('https://api.thetvdb.com/login', auth);
+	console.log('tv api response ', response);
+	return response.data.token;
 }
 
 /**
  * @description retrieves new token
  * @param {string} token contains the unexpired refresh token
  */
-function retrieveRefreshToken(token) {
-  const url = `https://api.thetvdb.com/refresh_token`;
+async function retrieveRefreshToken(token) {
+	const url = `https://api.thetvdb.com/refresh_token`;
+	const headers = {
+		headers: {
+			Authorization: `Bearer ${token}`
+		}
+	};
 
-  const headers = {
-    headers: {
-      Authorization: `Bearer ${token}`
-    }
-  };
-
-  axios.get(url, headers)
-      .then((response) => {
-        // console.log(response.data.token);
-        return response.data;
-      })
-      .catch((err) => {
-        console.log(err);
-        return err;
-      });
+	try {
+		return await axios.get(url, headers).data;
+	} catch (err) {
+		return err;
+	}
 }
 
 /**
@@ -54,33 +43,33 @@ function retrieveRefreshToken(token) {
  * @param {string} token
  * @return {object} response or error
  */
-function retrieveShowOverview(show, token) {
-  return new Promise((resolve, reject) => {
-    const returnedResponse = {};
-    const headers = {
-      headers: {
-        Authorization: `Bearer ${token}`
-      }
-    };
-    const url = `https://api.thetvdb.com/search/series?name=${show}`;
-    axios.get(url, headers)
-        .then((response) => {
-          returnedResponse.status = response.status;
-          response.data.data.forEach((elem, i, arr) => {
-            if (elem && elem.banner) {
-              arr[i].banner = `https://www.thetvdb.com/banners/${elem.banner}`;
-            }
-          });
-          returnedResponse.data = response.data.data;
-          resolve(returnedResponse);
-        })
-        .catch((err) => {
-          console.log('error ', err);
-          returnedResponse.status = response.status;
-          returnedResponse.error = response.data.Error;
-          reject(returnedResponse);
-        });
-  });
+async function retrieveShowOverview(show, token) {
+	const returnedResponse = {};
+	const headers = {
+		headers: {
+			Authorization: `Bearer ${token}`
+		}
+	};
+
+	try {
+		const response = await axios.get(
+			`https://api.thetvdb.com/search/series?name=${show}`,
+			headers
+		);
+		returnedResponse.status = response.status;
+
+		response.data.data.forEach((elem, i, arr) => {
+			if (elem && elem.banner) {
+				arr[i].banner = `https://www.thetvdb.com/banners/${elem.banner}`;
+			}
+		});
+		returnedResponse.data = response.data.data;
+		return returnedResponse;
+	} catch (err) {
+		console.log('error ', err);
+		returnedResponse.status = err.status;
+		returnedResponse.error = err.data.Error;
+	}
 }
 
 /**
@@ -89,35 +78,33 @@ function retrieveShowOverview(show, token) {
  * @param {string} token
  * @return {Promise}
  */
-function retrieveImages(seriesId, token) {
-  return new Promise((resolve, reject) => {
-    const returnedResponse = {};
-    // contains options to search by certain resolutions and image types
-    // const url = `https://api.thetvdb.com/series/${seriesId}/images/query?keyType=series&subkey=graphical`; // returns banners
-    const url = `https://api.thetvdb.com/series/${seriesId}/images/query?keyType=poster`; // returns large posters
-    // const url = `https://api.thetvdb.com/series/${seriesId}/images/query/params`; // get params needed for image call
-    // https://www.thetvdb.com/banners/ - add filename from this call to get the image for the show
+async function retrieveImages(seriesId, token) {
+	const returnedResponse = {};
+	// contains options to search by certain resolutions and image types
+	// const url = `https://api.thetvdb.com/series/${seriesId}/images/query?keyType=series&subkey=graphical`; // returns banners
+	const url = `https://api.thetvdb.com/series/${seriesId}/images/query?keyType=poster`; // returns large posters
+	// const url = `https://api.thetvdb.com/series/${seriesId}/images/query/params`; // get params needed for image call
+	// https://www.thetvdb.com/banners/ - add filename from this call to get the image for the show
 
-    const options = {
-      method: 'GET',
-      headers: { 'Authorization': `Bearer ${token}` },
-      url
-    };
+	const options = {
+		method: 'GET',
+		headers: { Authorization: `Bearer ${token}` },
+		url
+	};
 
-    axios(options)
-        .then((response) => {
-          returnedResponse.status = 200;
-          returnedResponse.data = response.data.data;
-          console.log('image response ', returnedResponse.data);
-          resolve(returnedResponse);
-        })
-        .catch((err) => {
-          console.log('error ', err);
-          returnedResponse.status = 500;
-          returnedResponse.error = err;
-          reject(returnedResponse);
-        });
-  });
+	try {
+		const response = await axios(options);
+		returnedResponse.status = 200;
+		returnedResponse.data = response.data.data;
+		console.log('image response ', returnedResponse.data);
+
+		return returnedResponse;
+	} catch (err) {
+		console.log('error ', err);
+		returnedResponse.status = 500;
+		returnedResponse.error = err;
+		return returnedResponse;
+	}
 }
 
 /**
@@ -126,34 +113,30 @@ function retrieveImages(seriesId, token) {
  * @param {string} token
  * @return {Promise}
  */
-function retrieveEpisodes(seriesId, token) {
-  return new Promise((resolve, reject) => {
-    const returnedResponse = {};
-    const headers = {
-      headers: {
-        Authorization: `Bearer ${token}`
-      }
-    };
+async function retrieveEpisodes(seriesId, token) {
+	const returnedResponse = {};
+	const headers = {
+		headers: {
+			Authorization: `Bearer ${token}`
+		}
+	};
 
-    // const url = `https://api.thetvdb.com/episodes/${episodeId}`;
-    const url = `https://api.thetvdb.com/series/${seriesId}/episodes`;
-    axios.get(url, headers)
-        .then((response) => {
-          console.log('response ', response);
-          // return response;
-          returnedResponse.status = response.status;
-          returnedResponse.data = response.data;
-          resolve(returnedResponse);
-        })
-        .catch((err) => {
-          console.log('error ', err);
-          err.err = err;
-          // return response;
-          returnedResponse.status = response.status;
-          returnedResponse.error = err.data.Error;
-          reject(returnedResponse);
-        });
-  });
+	// const url = `https://api.thetvdb.com/episodes/${episodeId}`;
+	const url = `https://api.thetvdb.com/series/${seriesId}/episodes`;
+
+	try {
+		const response = await axios.get(url, headers);
+		console.log('response ', response);
+		returnedResponse.status = response.status;
+		returnedResponse.data = response.data;
+		return returnedResponse;
+	} catch (err) {
+		console.log('error ', err);
+		err.err = err;
+		returnedResponse.status = err.status;
+		returnedResponse.error = err.data.Error;
+		return returnedResponse;
+	}
 }
 
 /**
@@ -162,37 +145,36 @@ function retrieveEpisodes(seriesId, token) {
  * @param {string} token
  * @return {Promise}
  */
-function retrieveActors(seriesId, token) {
-  return new Promise((resolve, reject) => {
-    const returnedResponse = {};
-    const headers = {
-      headers: {
-        Authorization: `Bearer ${token}`
-      }
-    };
+async function retrieveActors(seriesId, token) {
+	const returnedResponse = {};
+	const headers = {
+		headers: {
+			Authorization: `Bearer ${token}`
+		}
+	};
 
-    const url = `https://api.thetvdb.com/series/${seriesId}/actors`;
-    axios.get(url, headers)
-        .then((response) => {
-          console.log('response ', response.data.data);
-          returnedResponse.status = response.status;
-          returnedResponse.data = response.data.data;
-          resolve(returnedResponse);
-        })
-        .catch((err) => {
-          console.log('error ', err);
-          returnedResponse.status = err.status;
-          returnedResponse.error = err.data.Error;
-          reject(returnedResponse);
-        });
-  });
+	try {
+		const response = await axios.get(
+			`https://api.thetvdb.com/series/${seriesId}/actors`,
+			headers
+		);
+		console.log('response ', response.data.data);
+		returnedResponse.status = response.status;
+		returnedResponse.data = response.data.data;
+		return returnedResponse;
+	} catch (err) {
+		console.log('error ', err);
+		returnedResponse.status = err.status;
+		returnedResponse.error = err.data.Error;
+		return returnedResponse;
+	}
 }
 
 module.exports = {
-  tvShowLogin,
-  retrieveRefreshToken,
-  retrieveShowOverview,
-  retrieveImages,
-  retrieveEpisodes,
-  retrieveActors
+	tvShowLogin,
+	retrieveRefreshToken,
+	retrieveShowOverview,
+	retrieveImages,
+	retrieveEpisodes,
+	retrieveActors
 };
